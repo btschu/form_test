37d207a78ca0b1530ee099d9b78a2b60db59ad19
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-21 22:05:43 -0700

    added more concepts

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect # added request
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
 # our index route will handle rendering our form
 @app.route('/')
 def index():
@@ -10,9 +12,14 @@ def index():
 def create_user():
     print("Post Info Received")
     print(request.form)
-    # Never render a template on a POST request.
-    # Instead we will redirect to our index route.
-    return redirect('/')
+    # Here we add two properties to session to store the name and email
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/show')
+
+@app.route('/show')
+def show_user():
+    return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
 
 if __name__ == "__main__":
     app.run(debug=True)
